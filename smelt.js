854f70d8b19c19f9a3b835a9155b0630023f2da7
chanyong.moon <chanyong.moon@navercorp.com>
2019-11-25 02:53:33 +0900

    cc

--- smelt.js
+++ smelt.js
@@ -1,7 +1,12 @@
 #!/bin/env node
 
 const fs = require('fs');
+const replacer = str => {
+  if (str.indexOf('linkedin') > 0 || str.indexOf('github') > 0) return str.slice(0, str.length - 1) + ' target="tab"' + ">";
+  else return str.slice(0, str.length - 1) + ' target="content"' + ">";
+}
 
 const html = fs.readFileSync("./pages/Describing Blog.html").toString();
 
-fs.writeFileSync("./pages/Describing Blog.html", html.replace(/<a h/g, '<a target="content" h'));
+fs.writeFileSync("./pages/Describing Blog.html",
+html.replace(/<\s*a [^>]*>/g, replacer));
